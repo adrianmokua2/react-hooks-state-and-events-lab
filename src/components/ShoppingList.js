abfec8199ccbf1f7cfb9dd2f2ec607885aec6081
Adrian <adrian.mokua@icloud.com>
2023-10-28 15:40:10 +0300

    done

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -2,10 +2,16 @@ import React from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [selectedCategory, setSelectedCategory]=useState("All")
+  const changeCategory=(event)=>{
+    setSelectedCategory(event.target.value)
+  }
+  
+  const filteredItems = selectedCategory === 'All'? items : items.filter(item => item.category === selectedCategory);
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select onChange={changeCategory} value={selectedCategory} name="filter">
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
